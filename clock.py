d814023c3bbf0a78997ee3483ce8aed24a20c58b
Kozim <Abduraximovkozim@mail.ru>
2026-03-10 15:36:45 +0500

    fix timezone

--- clock.py
+++ clock.py
@@ -2,16 +2,19 @@ import asyncio
 from datetime import datetime
 from telethon import TelegramClient
 from telethon.tl.functions.account import UpdateProfileRequest
+import pytz
 
 api_id = 37369639
 api_hash = "0cbea3745b46f65f9abea27049246da3"
 
 client = TelegramClient("session", api_id, api_hash)
 
+tz = pytz.timezone("Asia/Tashkent")
+
 async def main():
     while True:
-        time = datetime.now().strftime("🕐 %H:%M")
-        await client(UpdateProfileRequest(first_name=time))
+        now = datetime.now(tz).strftime("🕐 %H:%M")
+        await client(UpdateProfileRequest(first_name=now))
         await asyncio.sleep(60)
 
 async def start():
