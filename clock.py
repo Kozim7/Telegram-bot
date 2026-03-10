11c8d6fa753e90b79f312f1a245c9b02601d18c2
Kozim <70709126+Kozim7@users.noreply.github.com>
2026-03-10 11:12:06 +0500

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,10 +1,7 @@
 import asyncio
-asyncio.set_event_loop(asyncio.new_event_loop())
-
+from datetime import datetime
 from telethon import TelegramClient
 from telethon.tl.functions.account import UpdateProfileRequest
-import asyncio
-from datetime import datetime
 
 api_id = 37369639
 api_hash = "0cbea3745b46f65f9abea27049246da3"
@@ -13,9 +10,12 @@ client = TelegramClient("session", api_id, api_hash)
 
 async def main():
     while True:
-        time = datetime.now().strftime("%H:%M")
-        await client(UpdateProfileRequest(first_name=time))
+        now = datetime.now().strftime("%H:%M")
+        await client(UpdateProfileRequest(about=f"🕒 {now}"))
         await asyncio.sleep(60)
 
-with client:
-    client.loop.run_until_complete(main())
+async def start():
+    await client.start()
+    await main()
+
+asyncio.run(start())
